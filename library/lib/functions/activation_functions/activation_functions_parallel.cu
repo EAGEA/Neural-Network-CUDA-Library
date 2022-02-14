bbc771e1ea5e286101b6fe2c3bdd97bbb9cd8790
Hugo-Bantignies <bantignieshugo@hotmail.fr>
2022-02-14 17:01:12 +0100

    Soft max derivative (not working yet) + new helper for softmax (to cover the reduction)

--- library/lib/functions/activation_functions/activation_functions_parallel.cu
+++ library/lib/functions/activation_functions/activation_functions_parallel.cu
@@ -144,51 +144,50 @@ __global__ void __kernel_tanh_derivative(float *results, float *inputs,
 }
 
 __global__ void __kernel_softmax(float *results, float *inputs,
-                                 size_t notused,size_t size)
+                                 size_t notused,size_t size, float *sum)
 {
     size_t index = blockIdx.x * blockDim.x + threadIdx.x;
-    float sum = .0f;
+    float ssum = .0f;
     // Check if the thread is in the matrix dimensions.
     if (index < size)
     {
         for(int i = 0; i < size; i++)
         {
-            sum+=exp(inputs[i]);
+            ssum+=exp(inputs[i]);
         }
 
-        results[index] = exp(inputs[index])/sum;
+        results[index] = exp(inputs[index])/ssum;
         __syncthreads();
     }
 }
 
 __global__ void __kernel_softmax_derivative(float *results, float *inputs,
-                                         size_t notused, size_t size)
+                                         size_t notused, size_t size,float *sum)
 {
     //REDUCTION OF EXP(X)
 
     //Reduction initialization
-    extern __shared__ int sharedsum[];
-    int tid = threadIdx.x;
+    extern __shared__ float sharedsum[];
+    unsigned int tid = threadIdx.x;
     size_t col = blockIdx.x * blockDim.x + tid;
-    size_t row = blockIdx.y * blockDim.y + threadIdx.y;
-    size_t index = row * size + col;
 
     //Copy into shared memory
-    //sharedsum[tid] = sum[col];
+    sharedsum[tid] = inputs[col];
     __syncthreads();
 
     for(int stride = blockDim.x/2; stride > 0; stride >>= 1)
     {
         if(tid < stride)
         {
-            //sharedsum[tid] += sharedsum[tid + stride];
+            sharedsum[tid] += sharedsum[tid + stride];
         }
     }
     __syncthreads();
-    //if(tid == 0) sum[blockIdx.x] = sharedsum[0];
+    if(tid == 0) sum[0] = sharedsum[0];
 
     //DERIVATIVE OF SOFTMAX
-    float sum = .0f;
+    size_t row = blockIdx.y * blockDim.y + threadIdx.y;
+    size_t index = row * size + col;
     float softmax_x = .0f;
     float softmax_y = .0f;
 
@@ -196,9 +195,9 @@ __global__ void __kernel_softmax_derivative(float *results, float *inputs,
     if (row < size && col < size)
     {
         //S(x)
-        softmax_x = exp(inputs[row])/sum;
+        softmax_x = exp(inputs[row])/sum[0];
         //S(y)
-        softmax_y = exp(inputs[col])/sum;
+        softmax_y = exp(inputs[col])/sum[0];
         // S(x) * (1 - S(x))
         if(row == col)
         {
@@ -235,6 +234,39 @@ void __helper(const matrix &results, const matrix &inputs,
     // Retrieve/free data from device.
     matrix_parallel::end_operation(results, &device_data1);
     matrix_parallel::end_operation(inputs, &device_data2);
+}
+
+void __helper_softmax(const matrix &results, const matrix &inputs,
+              void (kernel)(float *result, float *inputs, size_t nb_rows, size_t nb_cols, float *sum))
+{
+    auto cuda_dims = util::get_cuda_dims(inputs.get_dimensions());
+    auto block_dims = cuda_dims.first;
+    auto thread_dims = cuda_dims.second;
+
+    //Sum init for softmax
+    float sum = .0f;
+
+    float *device_data1;
+    float *device_data2;
+    float *device_sum;
+
+    // Prepare data on device.
+    matrix_parallel::start_operation(results, &device_data1);
+    matrix_parallel::start_operation(inputs, &device_data2);
+    cudaMalloc(&device_sum,1 * sizeof(float));
+    cudaMemcpy(device_sum,&sum,sizeof(float),cudaMemcpyHostToDevice);
+
+    // Do computations with CUDA threads.
+    kernel<<<block_dims, thread_dims>>>(
+            device_data1, device_data2,
+            results.get_dimensions().first, results.get_dimensions().second,device_sum);
+    // Wait for all threads.
+    CUDA_CHECK(cudaDeviceSynchronize());
+    // Retrieve/free data from device.
+    matrix_parallel::end_operation(results, &device_data1);
+    matrix_parallel::end_operation(inputs, &device_data2);
+    cudaMemcpy(&sum,device_sum,sizeof(float),cudaMemcpyDeviceToHost);
+    cudaFree(&device_sum);
 }
 
 
@@ -295,10 +327,10 @@ void activation_functions_parallel::tanh_derivative(std::vector<matrix *> m)
 
 void activation_functions_parallel::softmax(std::vector<matrix *> m)
 {
-    __helper(*m[0], *m[1],__kernel_softmax);
+    __helper_softmax(*m[0], *m[1],__kernel_softmax);
 }
 
 void activation_functions_parallel::softmax_derivative(std::vector<matrix *> m)
 {
-    __helper(*m[0], *m[1],__kernel_softmax_derivative);
+    __helper_softmax(*m[0], *m[1],__kernel_softmax_derivative);
 }
