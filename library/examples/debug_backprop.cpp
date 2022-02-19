4ad7c966c1887ca936ba3a133b34d82d905ec071
Hugo-Bantignies <bantignieshugo@hotmail.fr>
2022-02-19 21:29:52 +0100

    First test with potential error when : Vector * Vector --> Matrix.

--- library/examples/debug_backprop.cpp
+++ library/examples/debug_backprop.cpp
@@ -4,10 +4,56 @@
 
 #include "lib/data_structures/matrix/matrix.h"
 
+//Weights dimensions of L1 (matrix)
+#define weights_x 8
+#define weights_y 4
+
+//Errors dimensions of L1 (vector)
+#define errors_x 4
+#define errors_y 1
+
+//Inputs dimensions of L0 (vector)
+#define inputs_x 8
+#define inputs_y 1
 
 using namespace cudaNN;
 
 int main(int argc, char *argv[])
 {
-    auto m1 = matrix(8, 1, "1");
+    //Weights init for the layer L1
+    auto weights = matrix(weights_x, weights_y, "1");
+
+    for(int i = 0 ; i < weights_x ; i++)
+    {
+        for(int j = 0 ; j < weights_y; j++)
+        {
+            weights[i * weights_y + j] = 0.5;
+        }
+    }
+
+    //Errors init for the layer L1
+    auto errors = matrix(errors_x,errors_y,"2");
+
+    for(int i = 0 ; i < errors_x ; i ++)
+    {
+        errors[i] = 0.1;
+    }
+
+    //Inputs init (layer L0)
+    auto inputs = matrix(inputs_x,inputs_y,"3");
+
+    for(int i = 0 ; i < inputs_x ; i ++)
+    {
+        inputs[i] = 0.2;
+    }
+    //Print of the layer
+    std::cout << "BEFORE : weights \n";
+    matrix::print(weights);
+    std::cout << "BEFORE : errors \n";
+    matrix::print(errors);
+    std::cout << "BEFORE : inputs \n";
+    matrix::print(inputs);
+
+    auto mtest = errors * inputs.transpose();
+    matrix::print(mtest);
 }
