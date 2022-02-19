f48faacf729b44ac858a2a7ee31c611758893c87
Hugo-Bantignies <bantignieshugo@hotmail.fr>
2022-02-19 20:44:36 +0100

    Creation of an example file to help to debug the backpropagation.

---
+++ library/examples/debug_backprop.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hugo on 19/02/2022.
+//
+
+#include "lib/data_structures/matrix/matrix.h"
+
+
+using namespace cudaNN;
+
+int main(int argc, char *argv[])
+{
+    auto m1 = matrix(8, 1, "1");
+}
