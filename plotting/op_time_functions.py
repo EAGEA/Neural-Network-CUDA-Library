6f514661ab18da68fee27f3c72bd3628f795e531
Emilien AUFAUVRE <emilien.aufauvre@live.fr>
2022-02-24 01:22:45 +0100

    Added training loss + function execution time tests and plots.

---
+++ plotting/op_time_functions.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def print_data(csv_1, csv_2, ax, title):
+    # Uses the first column for the x axes.
+    csv_1.plot(x=csv_1.columns[0], marker='o', xticks=[1,4,7,10], ax=ax)
+    csv_2.plot(x=csv_2.columns[0], marker='x', xticks=[1,4,7,10], ax=ax)
+    # Set the bottom value to 0 for the Y axes.
+    ax.set_ylim(bottom=0)
+    # Set the title.
+    ax.set_title(title, fontsize=20)
+
+
+
+def main():
+    """
+    Print the CPU and GPU execution times obtained from
+    the "lib/examples/op_time_functions.cpp" test (that records
+    the execution time of every function).
+    The file obtained from the test should be placed in the "data/"
+    directory.
+    """
+    NB_OPERATIONS = 12 * 2
+    NB_ROWS = 4
+    NB_COLS = 6
+    OPERATIONS = ["Linear",
+                  "Linear derivative",
+                  "Binary Step",
+                  "Binary Step derivative",
+                  "Sigmoid",
+                  "Sigmoid derivative",
+                  "Relu",
+                  "Relu derivative",
+                  "Tanh",
+                  "Tanh derivative",
+                  "Softmax",
+                  "Softmax derivative",
+                  "MSE",
+                  "MSE derivative",
+                  "MAE",
+                  "MAE derivative",
+                  "MBE",
+                  "MBE derivative",
+                  "Hinge Loss",
+                  "Hinge Loss derivative",
+                  "BCE",
+                  "BCE derivative",
+                  "CE",
+                  "CE derivative"]
+    XLABEL = "Elements in the matrices (2^N)"
+    YLABEL = "Execution time (ms)"
+
+    fig, ax = plt.subplots(nrows=NB_ROWS, ncols=NB_COLS)
+    plt.tight_layout()
+
+    for i in range(0, NB_ROWS):
+        for j in range(0, NB_COLS):
+            index = i * NB_COLS + j
+            if index < NB_OPERATIONS:
+                data_1 = pd.read_csv("data/" + str(index) + "_functions_gpu.csv", delimiter=';')
+                data_2 = pd.read_csv("data/" + str(index) + "_functions_cpu.csv", delimiter=';')
+                print_data(data_1, data_2, ax[i][j], OPERATIONS[index]) 
+
+    # Set the labels.
+    plt.setp(ax[0, :], xlabel=XLABEL)
+    plt.setp(ax[1, :], xlabel=XLABEL)
+    plt.setp(ax[:, 0], ylabel=YLABEL)
+    # Full screen.
+    manager = plt.get_current_fig_manager()
+    manager.full_screen_toggle()
+    # Save into pdf.
+    plt.savefig("op_time_functions", format="pdf", dpi=1200)
+    # Show the graph.
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
--- plotting/op_time_matrices.py
+++ plotting/op_time_matrices.py
@@ -3,13 +3,9 @@ import pandas as pd
 
 
 def print_data(csv_1, csv_2, ax, title):
-    """
-    Use the data from the csv files to print two graphs; one
-    for the CPU execution time, and another for the GPU.
-    """
     # Uses the first column for the x axes.
-    csv_1.plot(x = csv_1.columns[0], marker='o', xticks=csv_1.iloc[:,0], ax=ax)
-    csv_2.plot(x = csv_2.columns[0], marker='x', xticks=csv_2.iloc[:,0], ax=ax)
+    csv_1.plot(x = csv_1.columns[0], marker='o', xticks=csv_1.iloc[:,0], legend = 0, ax=ax)
+    csv_2.plot(x = csv_2.columns[0], marker='x', xticks=csv_2.iloc[:,0], legend = 0, ax=ax)
     # Set the bottom value to 0 for the Y axes.
     ax.set_ylim(bottom=0)
     # Set the title.
@@ -20,10 +16,10 @@ def print_data(csv_1, csv_2, ax, title):
 def main():
     """
     Print the CPU and GPU execution times obtained from
-    the "lib/examples/op_time.cpp" test (that records
+    the "lib/examples/op_time_matrices.cpp" test (that records
     the execution time of every matrix operation).
-    The file obtained from the test should be placed in the "data/"
-    directory.
+    The file obtained from this test should be placed in the "data/"
+    directory. Then executing this script will produce a chart.
     """
     NB_OPERATIONS = 7
     NB_ROWS = 2
@@ -45,23 +41,23 @@ def main():
         for j in range(0, NB_COLS):
             index = i * NB_COLS + j
             if index < NB_OPERATIONS:
-                data_1 = pd.read_csv("data/" + str(index) + "_gpu.csv", delimiter = ';')
-                data_2 = pd.read_csv("data/" + str(index) + "_cpu.csv", delimiter = ';')
+                data_1 = pd.read_csv("data/" + str(index) + "_matrix_gpu.csv", delimiter=';')
+                data_2 = pd.read_csv("data/" + str(index) + "_matrix_cpu.csv", delimiter=';')
                 print_data(data_1, data_2, ax[i][j], OPERATIONS[index]) 
             else:
                 ax[i][j].set_visible(False)
 
+    # Legend.
+    ax.flatten()[-2].legend(loc="upper center", bbox_to_anchor=(2.25, 0.5), ncol=NB_COLS)
     # Set the labels.
     plt.setp(ax[0, :], xlabel=XLABEL)
     plt.setp(ax[1, :], xlabel=XLABEL)
     plt.setp(ax[:, 0], ylabel=YLABEL)
-    # To have a graph that can be easily included in another document.
-    plt.tight_layout(pad=0.05)
     # Full screen.
     manager = plt.get_current_fig_manager()
     manager.full_screen_toggle()
     # Save into pdf.
-    plt.savefig("op_time", format="pdf", dpi=1200)
+    plt.savefig("op_time_matrices", format="pdf", dpi=1200)
     # Show the graph.
     plt.show()
 
