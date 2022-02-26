5a4faf5ef6b20d7626806d249d867cdedf81d08a
Hugo-Bantignies <bantignieshugo@hotmail.fr>
2022-02-26 11:27:19 +0100

    Multiply in the neural net

--- library/lib/data_structures/matrix/matrix_parallel.cu
+++ library/lib/data_structures/matrix/matrix_parallel.cu
@@ -288,7 +288,7 @@ void matrix_parallel::multiply(const matrix &m,
     start_operation(m1, &device_data1);
     start_operation(m2, &device_data2);
     // Do computations with CUDA threads.
-    __kernel_tiled_multiply<<<block_dims, thread_dims,TILE_DIM * TILE_DIM * 2 * sizeof(float)>>>(
+    __kernel_multiply<<<block_dims, thread_dims,TILE_DIM * TILE_DIM * 2 * sizeof(float)>>>(
             device_result,
             device_data1, device_data2,
             m1.get_dimensions().first, m1.get_dimensions().second,
